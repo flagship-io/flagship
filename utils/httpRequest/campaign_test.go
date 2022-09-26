170322ece3d015d7e337d5817ea2d58e312346a2
chadiii <blackbeard@DESKTOP-E1T0NV5.localdomain>
2022-09-26 14:06:04 +0200

    add create test

--- utils/httpRequest/campaign_test.go
+++ utils/httpRequest/campaign_test.go
@@ -12,7 +12,29 @@ import (
 )
 
 type FlagKey struct {
-	Color string
+	Color string `json:"color"`
+}
+
+var scheduler = models.Scheduler{
+	StartDate: "2022-02-01 10:00:00",
+	StopDate:  "2022-02-02 08:00:00",
+	TimeZone:  "Europe/Paris",
+}
+
+var targetingGroupsTest = []models.TargetingGroup{
+	{
+		Targetings: []models.InnerTargeting{
+			{
+				Key:      "isVIP",
+				Operator: "CONTAINS",
+				Value:    true,
+			},
+		},
+	},
+}
+
+var targetingTest = models.Targeting{
+	TargetingGroups: targetingGroupsTest,
 }
 
 var variationTest = []models.Variation{
@@ -23,18 +45,18 @@ var variationTest = []models.Variation{
 		Modifications: models.Modification{
 			Type: "string",
 			Value: FlagKey{
-				Color: "red",
+				Color: "blue",
 			},
 		},
 	},
 	{
 		Name:       "My variation 2",
-		Reference:  true,
-		Allocation: 30,
+		Reference:  false,
+		Allocation: 50,
 		Modifications: models.Modification{
 			Type: "string",
 			Value: FlagKey{
-				Color: "blue",
+				Color: "red",
 			},
 		},
 	},
@@ -44,6 +66,7 @@ var variationGroupsTest = []models.VariationGroup{
 	{
 		Name:       "variationGroupName",
 		Variations: variationTest,
+		Targeting:  targetingTest,
 	},
 }
 
@@ -93,19 +116,27 @@ func TestHTTPListCampaign(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 
-	testProjectList := []models.Project{
+	testCampaignlist := []models.Campaign{
 		{
-			ID:   "1",
-			Name: "newTestingProject1",
+			ID:              "campaignID",
+			Name:            "newTestingCampaign",
+			ProjectID:       "projectIDTest",
+			Description:     "descriptionTest",
+			Type:            "toggle",
+			VariationGroups: variationGroupsTest,
 		},
 		{
-			ID:   "2",
-			Name: "newTestingProject2",
+			ID:              "campaignID2",
+			Name:            "newTestingCampaign1",
+			ProjectID:       "projectIDTest1",
+			Description:     "descriptionTest1",
+			Type:            "toggle",
+			VariationGroups: variationGroupsTest,
 		},
 	}
 
-	resp := HTTPListResponse[models.Project]{
-		Items:             testProjectList,
+	resp := HTTPListResponse[models.Campaign]{
+		Items:             testCampaignlist,
 		CurrentItemsCount: 2,
 		CurrentPage:       1,
 		TotalCount:        2,
@@ -113,7 +144,7 @@ func TestHTTPListCampaign(t *testing.T) {
 		LastPage:          1,
 	}
 
-	httpmock.RegisterResponder("GET", utils.Host+"/v1/accounts/"+viper.GetString("account_id")+"/projects",
+	httpmock.RegisterResponder("GET", utils.Host+"/v1/accounts/"+viper.GetString("account_id")+"/account_environments/"+viper.GetString("account_environment_id")+"/campaigns",
 		func(req *http.Request) (*http.Response, error) {
 			resp, err := httpmock.NewJsonResponse(200, resp)
 			if err != nil {
@@ -123,16 +154,22 @@ func TestHTTPListCampaign(t *testing.T) {
 		},
 	)
 
-	respBody, err := HTTPListProject()
+	respBody, err := HTTPListCampaign()
 
 	assert.NotNil(t, respBody)
 	assert.Nil(t, err)
 
-	assert.Equal(t, "1", respBody[0].ID)
-	assert.Equal(t, "newTestingProject1", respBody[0].Name)
+	assert.Equal(t, "campaignID", respBody[0].ID)
+	assert.Equal(t, "newTestingCampaign", respBody[0].Name)
+	assert.Equal(t, "projectIDTest", respBody[0].ProjectID)
+	assert.Equal(t, "descriptionTest", respBody[0].Description)
+	assert.Equal(t, "toggle", respBody[0].Type)
 
-	assert.Equal(t, "2", respBody[1].ID)
-	assert.Equal(t, "newTestingProject2", respBody[1].Name)
+	assert.Equal(t, "campaignID2", respBody[1].ID)
+	assert.Equal(t, "newTestingCampaign1", respBody[1].Name)
+	assert.Equal(t, "projectIDTest1", respBody[1].ProjectID)
+	assert.Equal(t, "descriptionTest1", respBody[1].Description)
+	assert.Equal(t, "toggle", respBody[1].Type)
 }
 
 func TestHTTPCreateCampaign(t *testing.T) {
@@ -141,14 +178,21 @@ func TestHTTPCreateCampaign(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 
-	testProject := models.Project{
-		ID:   "1",
-		Name: "newTestingProject",
+	testCampaign := models.Campaign{
+		ID:              "campaignID1",
+		Name:            "newTestingCampaign",
+		ProjectID:       "projectIDTest",
+		Description:     "descriptionTest",
+		Type:            "toggle",
+		VariationGroups: variationGroupsTest,
+		Scheduler:       scheduler,
 	}
 
-	httpmock.RegisterResponder("POST", utils.Host+"/v1/accounts/"+viper.GetString("account_id")+"/projects",
+	dataCampaign := "{\"project_id\":\"projectIDTest\",\"name\":\"newTestingCampaign\",\"description\":\"descriptionTest\",\"type\":\"toggle\",\"variation_groups\":[{\"name\":\"variationGroupName\",\"variations\":[{\"name\":\"My variation 1\",\"allocation\":50,\"reference\":true,\"modifications\":{\"value\":{\"color\":\"blue\"}}},{\"name\":\"My variation 2\",\"allocation\":50,\"reference\":false,\"modifications\":{\"value\":{\"color\":\"red\"}}}],\"targeting\":{\"targeting_groups\":[{\"targetings\":[{\"operator\":\"CONTAINS\",\"key\":\"isVIP\",\"value\":\"true\"}]}]}}],\"scheduler\":{\"start_date\":\"2022-02-01 10:00:00\",\"stop_date\":\"2022-02-02 08:00:00\",\"timezone\":\"Europe/Paris\"}}"
+
+	httpmock.RegisterResponder("POST", utils.Host+"/v1/accounts/"+viper.GetString("account_id")+"/account_environments/"+viper.GetString("account_environment_id")+"/campaigns",
 		func(req *http.Request) (*http.Response, error) {
-			resp, err := httpmock.NewJsonResponse(200, testProject)
+			resp, err := httpmock.NewJsonResponse(200, testCampaign)
 			if err != nil {
 				return httpmock.NewStringResponse(500, ""), nil
 			}
@@ -156,28 +200,36 @@ func TestHTTPCreateCampaign(t *testing.T) {
 		},
 	)
 
-	respBody, err := HTTPCreateProject(testProject.Name)
+	respBody, err := HTTPCreateCampaign(dataCampaign)
 
 	assert.NotNil(t, respBody)
 	assert.Nil(t, err)
 
-	assert.Equal(t, []byte("{\"id\":\"1\",\"name\":\"newTestingProject\"}"), respBody)
+	assert.Equal(t, []byte("{\"id\":\"campaignID1\",\"project_id\":\"projectIDTest\",\"name\":\"newTestingCampaign\",\"description\":\"descriptionTest\",\"type\":\"toggle\",\"status\":\"\",\"variation_groups\":[{\"id\":\"\",\"name\":\"variationGroupName\",\"variations\":[{\"id\":\"\",\"name\":\"My variation 1\",\"reference\":true,\"allocation\":50,\"modifications\":{\"type\":\"string\",\"value\":{\"color\":\"blue\"}}},{\"id\":\"\",\"name\":\"My variation 2\",\"reference\":false,\"allocation\":50,\"modifications\":{\"type\":\"string\",\"value\":{\"color\":\"red\"}}}],\"targeting\":{\"targeting_groups\":[{\"targetings\":[{\"key\":\"isVIP\",\"operator\":\"CONTAINS\",\"value\":true}]}]}}],\"scheduler\":{\"start_date\":\"2022-02-01 10:00:00\",\"stop_date\":\"2022-02-02 08:00:00\",\"timezone\":\"Europe/Paris\"}}"), respBody)
 }
 
 func TestHTTPEditCampaign(t *testing.T) {
+
 	ViperNotSet(t)
 
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 
-	testProject := models.Project{
-		ID:   "1",
-		Name: "newTestingProject",
+	testCampaign := models.Campaign{
+		ID:              "campaignID1",
+		Name:            "newTestingCampaign1",
+		ProjectID:       "projectIDTest1",
+		Description:     "descriptionTest1",
+		Type:            "toggle",
+		VariationGroups: variationGroupsTest,
+		Scheduler:       scheduler,
 	}
 
-	httpmock.RegisterResponder("PATCH", utils.Host+"/v1/accounts/"+viper.GetString("account_id")+"/projects/"+testProject.ID,
+	dataCampaign := "{\"project_id\":\"projectIDTest\",\"name\":\"newTestingCampaign\",\"description\":\"descriptionTest\",\"type\":\"toggle\",\"variation_groups\":[{\"name\":\"variationGroupName\",\"variations\":[{\"name\":\"My variation 1\",\"allocation\":50,\"reference\":true,\"modifications\":{\"value\":{\"color\":\"blue\"}}},{\"name\":\"My variation 2\",\"allocation\":50,\"reference\":false,\"modifications\":{\"value\":{\"color\":\"red\"}}}],\"targeting\":{\"targeting_groups\":[{\"targetings\":[{\"operator\":\"CONTAINS\",\"key\":\"isVIP\",\"value\":\"true\"}]}]}}],\"scheduler\":{\"start_date\":\"2022-02-01 10:00:00\",\"stop_date\":\"2022-02-02 08:00:00\",\"timezone\":\"Europe/Paris\"}}"
+
+	httpmock.RegisterResponder("PATCH", utils.Host+"/v1/accounts/"+viper.GetString("account_id")+"/account_environments/"+viper.GetString("account_environment_id")+"/campaigns",
 		func(req *http.Request) (*http.Response, error) {
-			resp, err := httpmock.NewJsonResponse(200, testProject)
+			resp, err := httpmock.NewJsonResponse(200, testCampaign)
 			if err != nil {
 				return httpmock.NewStringResponse(500, ""), nil
 			}
@@ -185,12 +237,12 @@ func TestHTTPEditCampaign(t *testing.T) {
 		},
 	)
 
-	respBody, err := HTTPEditProject(testProject.ID, testProject.Name)
+	respBody, err := HTTPCreateCampaign(dataCampaign)
 
 	assert.NotNil(t, respBody)
 	assert.Nil(t, err)
 
-	assert.Equal(t, []byte("{\"id\":\"1\",\"name\":\"newTestingProject\"}"), respBody)
+	assert.Equal(t, []byte("{\"id\":\"campaignID1\",\"project_id\":\"projectIDTest\",\"name\":\"newTestingCampaign\",\"description\":\"descriptionTest\",\"type\":\"toggle\",\"status\":\"\",\"variation_groups\":[{\"id\":\"\",\"name\":\"variationGroupName\",\"variations\":[{\"id\":\"\",\"name\":\"My variation 1\",\"reference\":true,\"allocation\":50,\"modifications\":{\"type\":\"string\",\"value\":{\"color\":\"blue\"}}},{\"id\":\"\",\"name\":\"My variation 2\",\"reference\":false,\"allocation\":50,\"modifications\":{\"type\":\"string\",\"value\":{\"color\":\"red\"}}}],\"targeting\":{\"targeting_groups\":[{\"targetings\":[{\"key\":\"isVIP\",\"operator\":\"CONTAINS\",\"value\":true}]}]}}],\"scheduler\":{\"start_date\":\"2022-02-01 10:00:00\",\"stop_date\":\"2022-02-02 08:00:00\",\"timezone\":\"Europe/Paris\"}}"), respBody)
 }
 
 func TestHTTPDeleteCampaign(t *testing.T) {
