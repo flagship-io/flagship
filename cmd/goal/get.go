41152560d8db763fe9b1df81a04c8f2ac18c3112
chadiii <blackbeard@DESKTOP-E1T0NV5.localdomain>
2022-12-13 18:37:55 +0100

    add get & list methods

---
+++ cmd/goal/get.go
@@ -0,0 +1,38 @@
+/*
+Copyright © 2022 Flagship Team flagship@abtasty.com
+
+*/
+package goal
+
+import (
+	"log"
+
+	"github.com/flagship-io/flagship/utils"
+	httprequest "github.com/flagship-io/flagship/utils/httpRequest"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// getCmd represents get command
+var getCmd = &cobra.Command{
+	Use:   "get [-i <goal-id> | --id=<goal-id>]",
+	Short: "Get a goal",
+	Long:  `Get a goal in your account`,
+	Run: func(cmd *cobra.Command, args []string) {
+		body, err := httprequest.HTTPGetGoal(GoalID)
+		if err != nil {
+			log.Fatalf("error occurred: %v", err)
+		}
+		utils.FormatItem([]string{"ID", "Label", "Type", "Operator", "Value"}, body, viper.GetString("output_format"), cmd.OutOrStdout())
+
+	},
+}
+
+func init() {
+	getCmd.Flags().StringVarP(&GoalID, "id", "i", "", "id of the goal you want to display")
+
+	if err := getCmd.MarkFlagRequired("id"); err != nil {
+		log.Fatalf("error occurred: %v", err)
+	}
+	GoalCmd.AddCommand(getCmd)
+}
---
+++ cmd/goal/list.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2022 Flagship Team flagship@abtasty.com
+
+*/
+package goal
+
+import (
+	"log"
+
+	"github.com/flagship-io/flagship/utils"
+	httprequest "github.com/flagship-io/flagship/utils/httpRequest"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all goals",
+	Long:  `List all goals in your account`,
+	Run: func(cmd *cobra.Command, args []string) {
+		body, err := httprequest.HTTPListGoal()
+		if err != nil {
+			log.Fatalf("error occurred: %v", err)
+		}
+		utils.FormatItem([]string{"ID", "Label", "Type", "Operator", "Value"}, body, viper.GetString("output_format"), cmd.OutOrStdout())
+	},
+}
+
+func init() {
+	GoalCmd.AddCommand(listCmd)
+}
