d08e732937e6a8bd8334e72e92c56298b89640c4
chadiii <blackbeard@DESKTOP-E1T0NV5.localdomain>
2022-12-13 17:10:04 +0100

    add goal http request

---
+++ utils/httpRequest/goal.go
@@ -0,0 +1,30 @@
+package httprequest
+
+import (
+	"net/http"
+
+	"github.com/flagship-io/flagship/models"
+	"github.com/flagship-io/flagship/utils"
+	"github.com/spf13/viper"
+)
+
+func HTTPListGoal() ([]models.Goal, error) {
+	return HTTPGetAllPages[models.Goal](utils.Host + "/v1/accounts/" + viper.GetString("account_id") + "/account_environments/" + viper.GetString("account_environment_id") + "/goals")
+}
+
+func HTTPGetGoal(id string) (models.Goal, error) {
+	return HTTPGetItem[models.Goal](utils.Host + "/v1/accounts/" + viper.GetString("account_id") + "/account_environments/" + viper.GetString("account_environment_id") + "/goals/" + id)
+}
+
+func HTTPCreateGoal(data string) ([]byte, error) {
+	return HTTPRequest(http.MethodPost, utils.Host+"/v1/accounts/"+viper.GetString("account_id")+"/account_environments/"+viper.GetString("account_environment_id")+"/goals", []byte(data))
+}
+
+func HTTPEditGoal(id, data string) ([]byte, error) {
+	return HTTPRequest(http.MethodPatch, utils.Host+"/v1/accounts/"+viper.GetString("account_id")+"/account_environments/"+viper.GetString("account_environment_id")+"/goals/"+id, []byte(data))
+}
+
+func HTTPDeleteGoal(id string) error {
+	_, err := HTTPRequest(http.MethodDelete, utils.Host+"/v1/accounts/"+viper.GetString("account_id")+"/account_environments/"+viper.GetString("account_environment_id")+"/goals/"+id, nil)
+	return err
+}
