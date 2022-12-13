d9b75b25202205113d919c25a16a7d2d4d23ca6d
chadiii <blackbeard@DESKTOP-E1T0NV5.localdomain>
2022-12-13 17:10:26 +0100

    add goal cmd

---
+++ cmd/goal/goal.go
@@ -0,0 +1,24 @@
+/*
+Copyright © 2022 Flagship Team flagship@abtasty.com
+
+*/
+package goal
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var (
+	GoalID  string
+	DataRaw string
+)
+
+// GoalCmd represents the goal command
+var GoalCmd = &cobra.Command{
+	Use:   "goal [create|edit|get|list|delete]",
+	Short: "Manage your goals",
+	Long:  `Manage your goals in your account`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
